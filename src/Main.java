1ffdec43d2df6d4a54ecc76f7a28afb6ca1f7e04
RobertsAsher5ae <rober5ae@mai.uc.edu>
2024-05-29 19:11:55 -0400

    intial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Initializes the values for the maintenance cost of all 4 seasons
+        double summerCost = 1000.90;
+        double autumnCost = 890.10;
+        double winterCost = 970.20;
+        double springCost = 700.50;
+
+        // Computes the total yearly maintenance cost
+        double totalCost = summerCost + autumnCost + winterCost + springCost;
+
+        // Outputs total cost and echoes each seasonal value
+        System.out.println(" ");
+        System.out.println("With a summer cost of $" + summerCost + ", an autum cost of $" + autumnCost +
+                           ", a winter cost of $" + winterCost + "\n" + "and a spring cost of $" + springCost +
+                           ", your total yearly home maintenance cost is $" + totalCost +"!");
+
+
 
     }
 }
